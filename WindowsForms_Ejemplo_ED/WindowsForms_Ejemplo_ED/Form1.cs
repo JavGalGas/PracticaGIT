5fff9963787911cd97e69e15c7aea156be704800
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-04-08 17:51:25 +0200

    Segunda version : añado código inicial, con errores

--- WindowsForms_Ejemplo_ED/WindowsForms_Ejemplo_ED/Form1.cs
+++ WindowsForms_Ejemplo_ED/WindowsForms_Ejemplo_ED/Form1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+//using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +12,32 @@ namespace WindowsForms_Ejemplo_ED
 {
     public partial class Form1 : Form
     {
+        private Button miBoton;
+        private TextBox miTextBox;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            miBoton = new Button();
+            miBoton.Size = new Size(70, 30);
+            miBoton.Location = new Point(30, 30);
+            miBoton.Text = "Haz clic";
+            miBoton.Click += new EventHandler(MiBotonClick);
+
+            miTextBox = new TextBox();
+            miTextBox.Size = new Size(200, 30);
+            miTextBox.Location = new Point(30, 70);
+
+            Controls.Add(miBoton);
+            Controls.Add(miTextBox);
+        }
+
+        //private void MiBotonClick(object sender, EventArgs e)
+        {
+            miTextBox.Text = "Has hecho clic en el botón!";
         }
     }
 }
